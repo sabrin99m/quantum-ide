b0ce788b852a109ad0fddcf6f37f9bc4a01cbeb3
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2025-11-08 03:41:32 +0100

    Add files via upload

---
+++ quantum-ide/quantum-ide-backend/src/test/java/com/example/quantum_ide_backend/QuantumIdeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quantum_ide_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumIdeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
